bab9fc3ccf30e41ff11c9d36f178d3240020b93c
Sharuk <msharuk.b10@gmail.com>
2024-06-05 18:17:09 +0530

    setuping frontend

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors'
 import dataRoute from './routes/route.js';
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 // app.use(express.urlencoded({extended : true}));
---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Section} from 'oolib'
+
+function App() {
+  return (
+    <>
+      <Section style={{backgroundColor: 'red', padding : '4px'}}>
+        
+      </Section>
+    </>
+  );
+}
+
+export default App;
